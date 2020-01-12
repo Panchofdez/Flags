9f39660e9b7af9687104490a431f596d44160d71
Pancho <panchofern@gmail.com>
2020-01-12 05:18:02 +0000

    project finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import FlagForm from './FlagForm';
+import worldImg from './world.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state={
+			countries:[],
+			options:[],
+			answer:[]
+		}
+		
+		this.randIndex=this.randIndex.bind(this);
+		this.gameSetUp=this.gameSetUp.bind(this);
+		this.resetGame=this.resetGame.bind(this);
+	}
+	componentDidMount(){
+		const url = 'https://restcountries.eu/rest/v2/all';
+		
+		fetch(url)
+			.then(data=>data.json())
+			.then(parsedData=>{
+				let gameData = this.gameSetUp(parsedData);
+				this.setState(
+					{
+						countries:parsedData,
+						options:gameData[0],
+						answer:gameData[1]
+					})
+			})
+			
+		
+	}
+	randIndex(array){
+		return Math.floor(Math.random()*array.length)
+		
+	}
+	gameSetUp(arr){
+		const options = [
+				arr[this.randIndex(arr)],
+				arr[this.randIndex(arr)],
+				arr[this.randIndex(arr)],
+				arr[this.randIndex(arr)]
+			]
+		const answer = options[Math.floor(Math.random()*options.length)];
+		return [options,answer]
+	}
+	resetGame(){
+		let newData=this.gameSetUp(this.state.countries);
+		this.setState({options:newData[0],answer:newData[1]});
+	}
+	
+		
+		
+	render(){
+		let gameForm = "Loading..."
+		if(this.state.countries && this.state.countries.length>0){
+			gameForm = <FlagForm {...this.state} resetGame={this.resetGame}/>
+		}
+		
+		return (
+			<div className="App">
+				<header
+					className="title-header"
+					style={{ backgroundImage: `url(${worldImg})` }}>
+					<h1 className="title-text">Guess The Flag</h1>
+				</header>
+				<div className='game'>
+					{gameForm}
+				</div>
+				<div className="flag">
+					<img src={this.state.answer.flag} alt="flag"/>
+				</div>
+			</div>
+		);
+	}
+
 }
-
+  
 export default App;
---
+++ src/FlagForm.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react';
+
+class FlagForm extends Component {
+	// static defaultProps ={
+	// 	resetGame(){}
+	// }
+	constructor(props){
+	super(props);
+	this.state=
+		{
+			selectedOption:undefined,
+			guessedCorrect:undefined
+			
+		}
+		this.handleOptionChange=this.handleOptionChange.bind(this);
+		this.handleSubmit=this.handleSubmit.bind(this);
+		this.restartGame=this.restartGame.bind(this);
+	}
+	handleOptionChange(event){
+		this.setState({selectedOption:event.target.value});
+	}
+	handleSubmit(event){
+		event.preventDefault();
+		if(this.state.selectedOption===this.props.answer.name){
+			this.setState({guessedCorrect:true})
+		}else{
+			this.setState({guessedCorrect:false})
+		}
+		
+	}
+	restartGame(){
+		this.props.resetGame();
+		this.setState(
+			{
+				selectedOption:undefined,
+				guessedCorrect:undefined
+			})
+	}
+	render(){
+		const {options}= this.props;
+		let	countryOptions = options.map((country,index)=>(
+					<div key={index}>
+						<label>
+							<input 
+								type="radio" 
+								checked={this.state.selectedOption===country.name} 
+								name="country" 
+								value={country.name}
+								onChange={this.handleOptionChange}/>
+							{country.name}
+						</label>
+					</div>
+			))
+		if (this.state.guessedCorrect===undefined){
+			return(
+				<form onSubmit={this.handleSubmit}>
+					{countryOptions}
+					<div>
+						<button type="submit">Guess</button>
+					</div>
+				</form>
+
+			)
+		}else if (this.state.guessedCorrect===true){
+			return (
+				<div>
+					<p>You Guessed Correctly! It was {this.state.selectedOption}</p>
+					<button onClick={this.restartGame}>Play Again</button>
+				</div>
+			)
+		}else{
+			return(
+				<div>
+					<p>You were wrong. The right answer was {this.props.answer.name}</p>
+					<button onClick={this.restartGame}>Try Again</button>
+				</div>
+			)
+		}	
+		
+	}
+}
+
+export default FlagForm;
